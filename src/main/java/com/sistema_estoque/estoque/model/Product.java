5921b4e36a53be1e914f040896973d1e10505282
KevinAp-5 <keven.clash3@gmail.com>
2025-02-22 14:14:51 -0300

    refact: Product model now using lombok to reduce boiler plate code

--- src/main/java/com/sistema_estoque/estoque/model/Product.java
+++ src/main/java/com/sistema_estoque/estoque/model/Product.java
@@ -9,9 +9,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "products")
 @Table(name="products")
+@Data
+@NoArgsConstructor
 @Builder
 public class Product {
 
@@ -24,45 +28,10 @@ public class Product {
     private BigDecimal price;
     private String category;
 
-    public Product() {}
-
     public Product(Long id, String name, BigDecimal price, String category) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.category = category;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
 }
